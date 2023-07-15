641f43bab622a4a14009075258af6779d829e6c9
itchaox <1462831573@qq.com>
2023-07-15 23:19:49 +0800

    chore: cSS 样式重置,集成 Sass 预处理器

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@
  * @Author     : itchaox
  * @Date       : 2023-07-15 15:15
  * @LastAuthor : itchaox
- * @LastTime   : 2023-07-15 18:35
+ * @LastTime   : 2023-07-15 23:09
  * @desc       :
  */
 import ReactDOM from 'react-dom/client';
 
-import '@/style/index.css';
+import '@/style/index.scss';
+import 'normalize.css';
 
 import App from '@/App.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+const app = ReactDOM.createRoot(document.getElementById('root')!);
+
+app.render(<App />);
